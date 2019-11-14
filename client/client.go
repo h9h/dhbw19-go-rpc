01efe36bb2d969f4eb7688f1b3e29294dcfd2106
Michael Heinke (SVI) <michael.heinke@sv-informatik.de>
2019-11-14 16:10:50 +0100

    Fix und Logging

--- client/client.go
+++ client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/rpc"
 )
@@ -9,18 +10,32 @@ type Args struct {
 	A, B int
 }
 
+type Quotient struct {
+	Quo, Rem int
+}
+
 func main() {
 	var err error
 	var reply int
-  
-  args := Args{7,8}
-  
+	var replyDivision Quotient
+
+	args := Args{25, 8}
+
 	// Create a TCP connection to localhost on port 1234
 	client, err := rpc.DialHTTP("tcp", "localhost:1234")
 	if err != nil {
 		log.Fatal("Connection error: ", err)
 	}
 
-	client.Call("Arith.Multiply", args, &reply)
-  fmt.Printf("Arith: %d*%d=%d", args.A, args.B, reply)
+	err = client.Call("Arith.Multiply", args, &reply)
+	if err != nil {
+		log.Fatal("Method Multiply Error: ", err)
+	}
+	fmt.Printf("Multiplikation: %d*%d=%d \n", args.A, args.B, reply)
+
+	err = client.Call("Arith.Divide", args, &replyDivision)
+	if err != nil {
+		log.Fatal("Method Divide Error: ", err)
+	}
+	fmt.Printf("Division mit Rest: %d/%d=%d Rest %d \n", args.A, args.B, replyDivision.Quo, replyDivision.Rem)
 }
--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net"
 	"net/http"
@@ -18,11 +19,13 @@ type Quotient struct {
 type Arith int
 
 func (t *Arith) Multiply(args *Args, reply *int) error {
+	log.Printf("Got call for Multiply with %d and %d \n", args.A, args.B)
 	*reply = args.A * args.B
 	return nil
 }
 
 func (t *Arith) Divide(args *Args, quo *Quotient) error {
+	log.Printf("Got call for Divide with %d and %d \n", args.A, args.B)
 	if args.B == 0 {
 		return errors.New("divide by zero")
 	}
@@ -32,26 +35,27 @@ func (t *Arith) Divide(args *Args, quo *Quotient) error {
 }
 
 func main() {
-  arith := new(Arith)
-	
-  // Publish the receivers methods
+	arith := new(Arith)
+
+	// Publish the server methods
 	err := rpc.Register(arith)
 	if err != nil {
-		log.Fatal("Format of service Arith isn't correct. ", err)
+		log.Fatal("Publiskinh server methods failed ", err)
 	}
 	// Register a HTTP handler
 	rpc.HandleHTTP()
+
 	// Listen to TPC connections on port 1234
 	listener, e := net.Listen("tcp", ":1234")
 	if e != nil {
 		log.Fatal("Listen error: ", e)
 	}
 
-  log.Printf("Serving RPC server on port %d", 1234)
+	log.Printf("Serving RPC server methods on port %d \n", 1234)
 
-  // Start accept incoming HTTP connections
-  err = http.Serve(listener, nil)
-  if err != nil {
-	  log.Fatal("Error serving: ", err)
-  }
+	// Start accept incoming HTTP connections
+	err = http.Serve(listener, nil)
+	if err != nil {
+		log.Fatal("Error serving: ", err)
+	}
 }
